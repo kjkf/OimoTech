5df7d518aebc4682535ba78cae7fc99b31111181
Lola <yakjkf@gmail.com>
2020-05-25 21:05:26 +0600

    light box gallery in mobile

--- src/js/lightboxGallery.js
+++ src/js/lightboxGallery.js
@@ -3,7 +3,7 @@
     var slideIndex = 1;
     const exampleSliders = document.getElementById('multi-item-example');
     const windowInner = window.visualViewport ? window.visualViewport.width : window.innerWidth;
-    if (exampleSliders && windowInner > 576) {
+    if (exampleSliders) {
 
         exampleSliders.addEventListener('click', e => {
             if (e.target.tagName !== 'IMG') return;
