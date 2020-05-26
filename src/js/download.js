d73dbad84fb7d1f8c34b7b4ee8578b14e983c3cd
Lola <yakjkf@gmail.com>
2020-05-26 15:47:50 +0600

    download price

--- src/js/download.js
+++ src/js/download.js
@@ -19,3 +19,25 @@ document.getElementById('downloadPrice').addEventListener('click', e => {
 
     download(filename);
 });
+
+document.getElementById('download-price').addEventListener('click', e => {
+    let filename = 'price.pdf';
+    const phoneNum = document.getElementById('phoneNum2');
+    console.log('111', phoneNum.value, isPhoneNum(phoneNum.value));
+    if (!isPhoneNum(phoneNum.value)) return;
+    download(filename);
+});
+
+document.getElementById('priceDownload').addEventListener('click', e => {
+    const phoneNum = document.getElementById('phoneNum');
+    console.log(2222, phoneNum.value, isPhoneNum(phoneNum.value));
+    if (!isPhoneNum(phoneNum.value)) return;
+    let filename = 'price.pdf';
+
+    download(filename);
+});
+
+function isPhoneNum(value) {
+    //console.log(value.indexOf('_'));
+    return value && value.indexOf('_') === -1;
+}
--- src/js/masks.js
+++ src/js/masks.js
@@ -8,8 +8,8 @@
     $(".phoneDownload").mask("+ 996 (ddd) dd dd dd", {
         autoclear: false,
         completed: function () {
-            download('price.pdf');
-            console.log('after download');
+            //download('price.pdf');
+            //console.log('after download');
         }
     });
 })();
